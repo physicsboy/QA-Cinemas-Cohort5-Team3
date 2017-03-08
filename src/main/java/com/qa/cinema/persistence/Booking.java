469bd3b194f6bed8beaf0584bb1f4544954c071d
jetbooster <jarvisam@gmail.com>
2017-03-08 11:26:35 +0000

    QCB-29 converted pojo to entity

--- src/main/java/com/qa/cinema/persistence/Booking.java
+++ src/main/java/com/qa/cinema/persistence/Booking.java
@@ -2,14 +2,41 @@ package com.qa.cinema.persistence;
 
 import java.util.Date;
 import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
 import com.qa.cinema.persistence.Ticket;
 
+
+@Entity
 public class Booking {
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="booking_id")
 	private Long bookingId;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name="booking_date_booked")
 	private Date dateBooked;
+	
+	@OneToMany(fetch = FetchType.LAZY)
+	@JoinColumn(name="ticket_booking_id")
 	private List<Ticket> tickets;
+	
+	@Column(name="booking_payment_email")
 	private String paymentEmail;
+	
+	@Column(name="booking_user_email")
 	private String userEmail;
 	
 	public Booking(){
