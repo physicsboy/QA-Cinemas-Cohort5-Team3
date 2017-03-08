a6e92af5d19265678129dcaa5949f1edef0704aa
OdysseyScorpio <martyn.robert.green@gmail.com>
2017-03-08 12:40:44 +0000

    QCB-21-ALL-DALS + app-state-manager

--- src/main/webapp/app/app-state-manager.js
+++ src/main/webapp/app/app-state-manager.js
@@ -1,20 +1,34 @@
 "use strict";
 
-(function () {
+(function() {
 
-    angular.module('movieApp').config(function ($stateProvider, $urlRouterProvider) {
+angular
+.module('movieApp')
+.config(
+function($stateProvider, $urlRouterProvider) {
 
-        $urlRouterProvider.otherwise("/getmovie");
+$urlRouterProvider.otherwise("/dashboard");
 
-        $stateProvider.state("dashboard", {
-            url: "/dashboard",
-            templateUrl: "app/feature/dashboard/dashboard-partial.html"
-        }).state("addmovie", {
-            url: "/addmovie",
-            templateUrl: "app/feature/movie/add/add-movie-partial.html"
-        }).state("getmovie", {
-            url: "/getmovie",
-            templateUrl: "app/feature/movie/get/get-movie-partial.html"
-        })
-    });
+$stateProvider.state("dashboard",{
+url : "/dashboard",
+templateUrl : "app/feature/dashboard/dashboard-partial.html"
+}).state("getmovie",{url : "/getmovie",
+templateUrl : "app/feature/movie/get-movie/get-movie.html"
+}).state("getbooking",{url : "/getbooking",
+templateUrl : "app/feature/booking/get-booking/get-booking.html"
+}).state("addbooking",{url : "/addbooking",
+templateUrl : "app/feature/booking/add-booking/add-booking.html"
+}).state("getscreen",{url : "/getscreen",
+templateUrl : "app/feature/screen/get-screen/get-screen.html"
+}).state("getticket",{url : "/getticket",
+templateUrl : "app/feature/ticket/get-ticket/get-ticket.html"
+}).state("adduser",{url : "/adduser",
+templateUrl : "app/feature/user/add-user/add-u.html"
+}).state("getuser",{url : "/getuser",
+templateUrl : "app/feature/user/get-user/add-u.html"
+}).state("getshowing",{url : "/getshowing",
+templateUrl : "app/feature/showing/get-showing/get-showing.html"
+})
+
+});
 }());
