ce6001ae61c9fda9479b7d12f05887674fc486ac
Alex <alex_newton@btinternet.com>
2017-03-08 15:45:24 +0000

    QCB-4 added controllers and updated ticket.dal

--- src/main/webapp/app/feature/ticket/delete-ticket/delete-ticket-controller.js
+++ src/main/webapp/app/feature/ticket/delete-ticket/delete-ticket-controller.js
@@ -1,1 +1,21 @@
 /** @author AlexNewton */
+
+(function() {
+
+    var DeleteTicketController =  function($state, ticketDal) {
+
+        var vm = this;
+
+
+        vm.deleteTicket = function(ticket){
+            ticketDal.deleteTicket(ticket).then(function () {
+                $state.go('addticket');
+            }, function (error) {
+                vm.error = true;
+                vm.errorMessage = error;
+            });
+        };
+    };
+
+    angular.module('movieApp').controller('deleteTicketController', [ '$state', 'ticketDal', DeleteTicketController]);
+}());
--- src/main/webapp/app/service/dal/ticket.js
+++ src/main/webapp/app/service/dal/ticket.js
@@ -2,25 +2,34 @@
 
 (function () {
 
-    angular.module("movieApp").service("ticketDal", ["dal", TicketDal]);
-
     function TicketDal (dal) {
 
-        this.getTickets = function () {
+        this.getAllTickets = function () {
             return dal.http.GET("rest/ticket/json");
         };
 
+        this.getAllTicketsForBooking = function(id){
+            return dal.http.GET("rest/ticket/json/booking/" + id)
+        };
+        
+        this.getTicketByID = function(id){
+        	return dal.http.GET("rest/ticket/json/" + id)
+        };
+
         this.saveTicket = function (ticketToSave) {
-            return dal.http.POST("rest/ticket/json",ticketToSave);
+            return dal.http.POST("rest/ticket/json", ticketToSave);
         };
 
-        this.updateTicket = function (ticketToUpdateId,ticketToUpdate) {
-            return dal.http.PUT("rest/ticket/json/" +ticketToUpdateId,ticketToUpdate);
+        this.updateTicket = function (ticketToUpdateId, ticketToUpdate) {
+            return dal.http.PUT("rest/ticket/json/" + ticketToUpdateId, ticketToUpdate);
         };
 
-        this.deleteTicket = function (ticketToDeleteId) {
-            return dal.http.DELETE("/rest/ticket/json/" +ticketToDeleteId);
+        this.deleteTicket = function (ticketToDelete) {
+            return dal.http.DELETE("/rest/ticket/json/", ticketToDelete);
         };
-
     }
+
+
+    angular.module("movieApp").service("ticketDal", ["dal", TicketDal]);
+
 }());
