9e4b03aafbc5182d52353daed2994797e440e930
jetbooster <jarvisam@gmail.com>
2017-03-08 12:55:51 +0000

    QCB-2 added controller.js,endpoint, html

---
+++ src/main/webapp/app/feature/booking/add/add-booking-controller.js
@@ -0,0 +1,26 @@
+/**
+ * @author Sam Jarvis
+ */
+
+(function() {
+
+    var AddBookingController =  function($state, bookingDal) {
+        var vm = this;
+
+        vm.addBooking = function(bookingToAdd) {
+            console.log("This is the value of booking to add " + bookingToAdd);
+            console.log(bookingToAdd);
+            var bookingToJson = JSON.stringify(bookingToAdd);
+            console.log(bookingToJson);
+            bookingDal.saveBooking(bookingToAdd).then(function (results) {
+                vm.bookingAddMessage  = results;
+                $state.go('getbooking');
+            }, function (error) {
+                vm.error = true;
+                vm.errorMessage = error;
+            });
+        };
+    };
+
+    angular.module('bookingApp').controller('addBookingController', ['$state','bookingDal',AddBookingController]);
+}());
