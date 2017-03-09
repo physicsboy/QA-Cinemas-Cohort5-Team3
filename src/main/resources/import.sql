320838b7c2ec04cdd92ee9029a8ef4f3168f68b5
Alex <alex_newton@btinternet.com>
2017-03-09 16:49:03 +0000

    QCB-60 import.sql finished for all current entities

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,9 +1,9 @@
 <!-- @author AlexNewton -->
 
-INSERT INTO movie VALUES (1, 'Some person', '12A', 'string', 'Action', 118, 'urlhere', 'Kong', '2017-03-10');
-INSERT INTO movie VALUES (2, 'An actor', '15', 'string', 'RomCom', 85, 'urlHere', '50  shades of Grey', '2015-03-10');
-INSERT INTO movie VALUES (3, 'Somebody', '18', 'string', 'Action', 118, 'urlHere', 'Kong', '2016-03-10');
-INSERT INTO movie VALUES (4, 'All Yall', '12A', 'string', 'Looooolll', 24, 'urlHere', 'American Horror Story', '2012-01-10');
+INSERT INTO movie VALUES (1, 'Ralph Finnnes, Maria Carey et Al', 'U', 'In a world full of lego..... there is batman', 'Adventure', 118, 'https://images-na.ssl-images-amazon.com/images/M/MV5BMTcyNTEyOTY0M15BMl5BanBnXkFtZTgwOTAyNzU3MDI@._V1_SY1000_CR0,0,674,1000_AL_.jpg', 'Lego Batman Movie', '2017-03-10');
+INSERT INTO movie VALUES (2, 'Leonardo DiCaprio, Joseph Gordon-Levitt, Ellen Page', '15', 'mystery and dreaming', 'Action, Sci-Fi', 148, 'https://images-na.ssl-images-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SY1000_CR0,0,675,1000_AL_.jpg', 'Inception', '2010-07-08');
+INSERT INTO movie VALUES (3, 'Tom Hiddleston, Samuel L. Jackson, Brie Larson', '18', 'Humanity tries to save itself from a rather large ape.', 'Action', 120, 'https://images-na.ssl-images-amazon.com/images/M/MV5BMTUwMzI5ODEwNF5BMl5BanBnXkFtZTgwNjAzNjI2MDI@._V1_SY1000_CR0,0,674,1000_AL_.jpg', 'Kong: Skull Island', '2017-03-01');
+INSERT INTO movie VALUES (4, 'Roy Scheider, Robert Shaw, Richard Dreyfuss', '12A', 'A big shark is eating people. The special effects are on point.', 'Adventure', 96, 'https://images-na.ssl-images-amazon.com/images/M/MV5BMmVmODY1MzEtYTMwZC00MzNhLWFkNDMtZjAwM2EwODUxZTA5XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX651_CR0,0,651,999_AL_.jpg', 'Jaws', '1975-01-10');
 
 INSERT INTO screen VALUES (1, 'N', 40),(2, 'P', 30),(3, 'P',10),(4, 'M', 15);
 
@@ -24,6 +24,6 @@ INSERT INTO booking VALUES (3, '2017-12-03', 'fred@fred.com', 'alex@alex.com');
 
 INSERT INTO ticket VALUES (1, 6.99, 'A1', 'Child', 1, 1),
 INSERT INTO ticket VALUES (2, 9.99, 'A3', 'Adult', 2, 2);
-INSERT INTO ticket VALUES (3, 5.99, 'A1', 'Child', 4, 2),
-INSERT INTO ticket VALUES (4, 7.99, 'A3', 'Concession', 3, 4);
-INSERT INTO ticket VALUES (5, 6.99, 'A3', 'Adult', 4, 4);
+INSERT INTO ticket VALUES (3, 5.99, 'F222', 'Child', 4, 2),
+INSERT INTO ticket VALUES (4, 7.99, 'N7', 'Concession', 3, 4);
+INSERT INTO ticket VALUES (5, 6.99, 'M15', 'Adult', 4, 4);
