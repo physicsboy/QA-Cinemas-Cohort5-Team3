2a3edf49cddf3b0941dfe5fe4345b0162e87b0f7
Alex <alex_newton@btinternet.com>
2017-03-10 17:35:34 +0000

    Progress made on creating booking/tickets

--- src/main/webapp/app/feature/movie/get/get-movie-controller.js
+++ src/main/webapp/app/feature/movie/get/get-movie-controller.js
@@ -1,21 +1,22 @@
-(function() {
+(function () {
 
-    var GetMovieController =  function(movieDal) {
+    var GetMovieController = function (movieDal) {
         var vm = this;
 
         vm.getMovies = function getMovies() {
             movieDal.getMovies().then(function (results) {
-                vm.movies  = results;
+                vm.movies = results;
             }, function (error) {
                 vm.error = true;
                 vm.errorMessage = error;
             });
         };
-        init();
+        vm.getMovies();
+
 
         vm.getMovieByTitle = function getMovieByTitle() {
             movieDal.getMovieByTitle().then(function (results) {
-                vm.movies  = results;
+                vm.movies = results;
             }, function (error) {
                 vm.error = true;
                 vm.errorMessage = error;
@@ -24,7 +25,7 @@
 
         vm.getMovieByGenre = function getMovieByGenre() {
             movieDal.getMovieByGenre().then(function (results) {
-                vm.movies  = results;
+                vm.movies = results;
             }, function (error) {
                 vm.error = true;
                 vm.errorMessage = error;
@@ -33,7 +34,7 @@
 
         vm.getMovieByYear = function getMovieByYear() {
             movieDal.getMovieByYear().then(function (results) {
-                vm.movies  = results;
+                vm.movies = results;
             }, function (error) {
                 vm.error = true;
                 vm.errorMessage = error;
