ea7259a2f3152b647a0bc1f959034c3e0fc27225
JamesLam1990 <james.lamkin@hotmail.co.uk>
2017-03-10 15:30:41 +0000

    QCB-6-Showing

--- src/main/java/com/qa/cinema/rest/ShowingEndPoint.java
+++ src/main/java/com/qa/cinema/rest/ShowingEndPoint.java
@@ -2,8 +2,12 @@ package com.qa.cinema.rest;
 
 
 import javax.inject.Inject;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
 import com.qa.cinema.service.ShowingService;
@@ -13,14 +17,47 @@ public class ShowingEndPoint {
 
 	
 	@Inject
-	private ShowingService showing;
+	private ShowingService service;
 	
 	@Path("/json")
 	@GET
 	@Produces({"application/json"})
 	public String getAllShowings(){
-		return showing.getAllShowings();
+		return service.getAllShowings();
 	}
 	
+	@Path("/json")
+	@GET
+	@Produces({"application/josn"})
+	public String getShowingByShowingId(@PathParam("id")Long showingId){
+		return service.getShowingById(showingId);
+	}
 	
+	@Path("/json")
+	@GET
+	@Produces({"application/josn"})
+	public String getShowingByMovieId(@PathParam("id")Long movieId){
+		return service.getShowingByMovie(movieId);
+	}
+	
+	@Path("/json")
+	@POST
+	@Produces({"application/json"})
+	public String addShowing(String showing){
+		return service.addShowing(showing);
+	}
+	
+	@Path("/json")
+	@PUT
+	@Produces({"application/json"})
+	public String updateShowing(@PathParam("id") Long showingId, String showing){
+		return service.updateShowing(showingId, showing);
+	}
+	
+	@Path("/json/{id}")
+	@DELETE
+	@Produces({ "application/json" })
+	public String deleteShowing(@PathParam("id") Long id) {
+		return service.deleteShowing(id);
+		}
 }
--- src/main/java/com/qa/cinema/service/ShowingServiceDBImpl.java
+++ src/main/java/com/qa/cinema/service/ShowingServiceDBImpl.java
@@ -26,8 +26,51 @@ public class ShowingServiceDBImpl implements ShowingService {
 	@Override
 	public String getAllShowings() {
 		Query query = manager.createQuery("SELECT s FROM Showing s");
+		@SuppressWarnings("unchecked")
 		Collection<Showing> showing = (Collection<Showing>) query.getResultList();
 		return util.getJSONForObject(showing);
+	}
+	
+	public String getShowingById(Long showingId){
+		Query query = manager.createQuery("SELECT s FROM Showing s WHERE id =" + showingId);
+		Showing showing = (Showing) query.getSingleResult();
+		return util.getJSONForObject(showing);
+	}
+	
+	public String getShowingByMovie(Long movieID){
+		Query query = manager.createQuery("SELECT s FROM Showing s WHERE movie.id =" + movieID);
+		@SuppressWarnings("unchecked")
+		Collection<Showing> showing = (Collection<Showing>) query.getResultList();
+		return util.getJSONForObject(showing);
+	}
+	
+	public String addShowing(String showing){
+		Showing newShowing = util.getObjectForJSON(showing, Showing.class);
+		manager.persist(newShowing);
+		return  "{\"message\": \"showing sucessfully added\"}";
+	}
+	
+	public String deleteShowing(Long id){
+		Showing showingToBeDeleted = findShowing(id);
+		if (showingToBeDeleted != null){
+			manager.remove(showingToBeDeleted);
+		}
+		
+		return "{\"message\": \"showing sucessfully deleted\"}";
+	}
+	
+	public String updateShowing(Long showingId, String showing){
+		Showing updatedShowing = util.getObjectForJSON(showing, Showing.class);
+		Showing showingInDB = findShowing(showingId);
+		if (showingInDB != null) {
+			showingInDB = updatedShowing;
+			manager.merge(showingInDB);
+			}
+		return "{\"message\": \"showing sucessfully updated\"}";
+	}
+	
+	private Showing findShowing(Long id) {
+		return manager.find(Showing.class, id);
 	}
 	
 }
