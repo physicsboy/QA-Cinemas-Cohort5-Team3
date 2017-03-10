b4c27d6b7620753a85db0e55f6137cee81a06e99
JamesLam1990 <james.lamkin@hotmail.co.uk>
2017-03-10 10:46:34 +0000

    QCB-6-Showing

---
+++ src/main/webapp/app/feature/showing/get-showing-controller.js
@@ -0,0 +1,20 @@
+/** @author James Lamkin */
+(function() {
+
+    var getShowingController =  function(showingDal) {
+        var vm = this;
+        vm.test = "test";
+
+        function init(){
+            showingDal.getShowing().then(function (results) {
+                vm.showing = results;
+            }, function (error) {
+                vm.error = true;
+                vm.errorMessage = error;
+            })
+        }
+        init();
+    };
+
+    angular.module('cinema').controller('getShowingController', [getShowingController]);
+}());
