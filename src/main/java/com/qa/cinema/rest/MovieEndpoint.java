5c2960af7c91df11b64f31abf50e821245d20d83
sjack5on <sjackson1601@gmail.com>
2017-03-10 09:43:51 +0000

    10/03/2017 push #1

--- src/main/java/com/qa/cinema/rest/MovieEndpoint.java
+++ src/main/java/com/qa/cinema/rest/MovieEndpoint.java
@@ -1,17 +1,21 @@
 package com.qa.cinema.rest;
 
 import javax.inject.Inject;
+import javax.ws.rs.DELETE;
 //import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 //import javax.ws.rs.POST;
 //import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 //import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
 import com.qa.cinema.service.MovieService;
 
-@Path("/movieApp/rest/movie")
+@Path("/movie")
 public class MovieEndpoint {
 
 	@Inject
@@ -23,39 +27,39 @@ public class MovieEndpoint {
 	public String getAllMovies() {
 		return service.listAllMovies();
 	}
-	
-	@Path("/json")
+
+	@Path("/title/json/{title}")
 	@GET
-	@Produces({"application/json"})
-	public String getMovieByTitle(String title) {
+	@Produces({ "application/json" })
+	public String getMovieByTitle(@PathParam("title") String title) {
 		return service.listMovieByTitle(title);
 	}
-	
-	@Path("/json")
+
+	@Path("/genre/json/{genre}")
 	@GET
-	@Produces({"application/json"})
-	public String getMovieByGenre(String genre) {
-		return service.listMovieByTitle(genre);
+	@Produces({ "application/json" })
+	public String getMovieByGenre(@PathParam("genre") String genre) {
+		return service.listMovieByGenre(genre);
 	}
 
-//	@Path("/json")
-//	@POST
-//	@Produces({ "application/json" })
-//	public String addMovie(String movie) {
-//		return service.createNewMovie(movie);
-//	}
-//
-//	@Path("/json/{id}")
-//	@PUT
-//	@Produces({ "application/json" })
-//	public String updateMovie(@PathParam("id") Long movieId, String movie) {
-//		return service.updateMovie(movieId, movie);
-//	}
-//
-//	@Path("/json/{id}")
-//	@DELETE
-//	@Produces({ "application/json" })
-//	public String deleteMovie(@PathParam("id") Long moviedId) {
-//		return service.deleteMovie(moviedId);
-//	}
+	@Path("/json")
+	@POST
+	@Produces({ "application/json" })
+	public String addMovie(String movie) {
+		return service.createNewMovie(movie);
+	}
+
+	@Path("/json/{id}")
+	@PUT
+	@Produces({ "application/json" })
+	public String updateMovie(@PathParam("id") Long movieId, String movie) {
+		return service.updateMovie(movieId, movie);
+	}
+
+	@Path("/json/{id}")
+	@DELETE
+	@Produces({ "application/json" })
+	public String deleteMovie(@PathParam("id") Long moviedId) {
+		return service.deleteMovie(moviedId);
+	}
 }
