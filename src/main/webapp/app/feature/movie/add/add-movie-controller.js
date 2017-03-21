0a8261a0247ddc94ab2c5a5deb1fdc2051cb1814
mattimate <matthew-gordon@hotmail.co.uk>
2017-03-21 12:22:31 +0000

    QCB-133-Create-Movie implemented functional add new movie page

--- src/main/webapp/app/feature/movie/add/add-movie-controller.js
+++ src/main/webapp/app/feature/movie/add/add-movie-controller.js
@@ -1,34 +1,25 @@
 (function() {
 
-    var AddMovieController =  function($state, movieDal, mediaDal) {
-        var vm = this;
+	var AddMovieController = function($state, movieDal) {
+		var vm = this;
 
-        vm.addMovie = function(movieToAdd, mediaToAdd) {
-            console.log("This is the value of movie to add ");
-            console.log(movieToAdd);
-            console.log("This is the value of media to add ");
-            console.log(mediaToAdd);
-            
-            var movieToJson = JSON.stringify(movieToAdd);
-            console.log(movieToJson);
-            var mediaToJson = JSON.stringify(mediaToAdd);
-            console.log(mediaToJson);
-            
-            movieDal.saveMovie(movieToAdd).then(function (results) {
-                vm.movieAddMessage  = results;
-            }, function (error) {
-                vm.error = true;
-                vm.errorMessage = error;
-            })
-            mediaDal.saveMedia(mediaToAdd).then(function (results) {
-            	vm.mediaAddMessage  = results;
-            	$state.go('getmovie');
-            }, function (error) {
-            	vm.error = true;
-            	vm.errorMessage = error;
-            });
-        };
-    };
+		vm.addMovie = function(movieToAdd) {
+			console.log("This is the value of movie to add ");
+			console.log(movieToAdd);
 
-    angular.module('movieApp').controller('addMovieController', ['$state','movieDal','mediaDal',AddMovieController]);
+			var movieToJson = JSON.stringify(movieToAdd);
+			console.log(movieToJson);
+
+			movieDal.saveMovie(movieToAdd).then(function(results) {
+				vm.movieAddMessage = results;
+				$state.go("getmovie"); //TODO Movie data needs to include media 
+			}, function(error) {
+				vm.error = true;
+				vm.errorMessage = error;
+			});
+		};
+	};
+
+	angular.module('movieApp').controller('addMovieController',
+			[ '$state', 'movieDal', AddMovieController ]);
 }());
