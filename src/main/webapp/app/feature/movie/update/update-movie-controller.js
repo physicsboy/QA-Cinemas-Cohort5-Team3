e59bbeb4b13d176850858441cfd3abaa122d0e68
mattimate <matthew-gordon@hotmail.co.uk>
2017-03-21 16:17:32 +0000

    QCB-135-Update-Movie implemented wipmovie  update function

---
+++ src/main/webapp/app/feature/movie/update/update-movie-controller.js
@@ -0,0 +1,24 @@
+/**
+ * @author Matt Gordon
+ */
+
+(function() {
+	var UpdateMovieController = function($state, movieDal) {
+		var vm = this;
+
+		vm.updateUser = function(movieToUpdate) {
+			console.log("This is the value of movie to Update..." + movieToUpdate);
+			console.log(movieToUpdate);
+			var movieToJson = JSON.stringify(movieToUpdate);
+			console.log(movieToJson);
+			movieDal.updateMovie(movieToUpdate).then(function(results) {
+				vm.movieUpdateMessage = results;
+				$state.go('getmovie');
+			}, function(error) {
+				vm.error = true;
+				vm.errorMessage = error;
+			});
+		};
+	};
+	angular.module('movieApp').controller('updateMovieController', [ '$state', 'movieDal', UpdateMovieController]);
+}());
