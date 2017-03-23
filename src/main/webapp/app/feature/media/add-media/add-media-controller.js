e5535d16911a0cf1d99081ae4370846a112802ee
mattimate <matthew-gordon@hotmail.co.uk>
2017-03-23 13:25:18 +0000

    QCB-1-Movie-Final Fixed delete functionality and fixed getmovie page

--- src/main/webapp/app/feature/media/add-media/add-media-controller.js
+++ src/main/webapp/app/feature/media/add-media/add-media-controller.js
@@ -1,3 +1,25 @@
-/**
- * Created by Administrator on 15/03/2017.
- */
+(function() {
+
+	var AddMediaController = function($state, mediaDal) {
+		var vm = this;
+
+		vm.addMedia = function(mediaToAdd) {
+			console.log("This is the value of media to add ");
+			console.log(mediaToAdd);
+
+			var mediaToJson = JSON.stringify(mediaToAdd);
+			console.log(mediaToJson);
+
+			mediaDal.saveMedia(mediaToAdd).then(function(results) {
+				vm.mediaAddMessage = results;
+				$state.go("getmedia");
+			}, function(error) {
+				vm.error = true;
+				vm.errorMessage = error;
+			});
+		};
+	};
+
+	angular.module('mediaApp').controller('addMediaController',
+			[ '$state', 'mediaDal', AddMediaController ]);
+}());
